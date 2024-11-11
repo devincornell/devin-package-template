f8953f49a11beec1c30b6bf8fc6ba4150e9dfeb1
Devin J. Cornell <devincornell@github.com>
2024-11-11 12:49:22 -0500

    adjusted import to pull from src

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,15 @@
 
-import src.mypkg as mypkg
-from src.mypkg import separate_funcs
+import sys
+sys.path.append('../src')
+
+import mypkg
+from mypkg import separate_funcs
 
 if __name__ == '__main__':
     
     # functions in root package namespace
-    so = mypkg.SmallType()
-    bo = mypkg.BigType()
+    so = mypkg.SmallType(1, 2)
+    bo = mypkg.BigType(1, 2, 3)
 
     # functions in submodule namespace
     mypkg.submodule.my_submodfunc()
